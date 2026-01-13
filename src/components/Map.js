ce85fd061ebb24845d989cecceaae9724ce59337
Winnie Palangpour <winnie.mabee@gmail.com>
2026-01-13 14:59:19 -0600

    Copilot 'Add a layer to the map component that renders a red heart on Austin, TX.'

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
-
 const MapComponent = () => {
   const mapContainerRef = useRef(null);
 
@@ -10,8 +9,50 @@ const MapComponent = () => {
     const map = new maplibregl.Map({
       container: mapContainerRef.current,
       style: 'https://demotiles.maplibre.org/style.json', // MapLibre demo style
-      center: [0, 0], // Initial map center [longitude, latitude]
-      zoom: 2, // Initial zoom level
+      center: [-97.7431, 30.2672], // Austin, TX [longitude, latitude]
+      zoom: 10, // Zoom level
+    });
+
+    map.on('load', () => {
+      // Add a GeoJSON source for the heart
+      map.addSource('austin-heart', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-97.7431, 30.2672], // Austin, TX coordinates
+              },
+              properties: {},
+            },
+          ],
+        },
+      });
+
+      // Add a layer to display the heart
+      map.addLayer({
+        id: 'austin-heart-layer',
+        type: 'symbol',
+        source: 'austin-heart',
+        layout: {
+          'icon-image': 'heart-icon', // Reference the custom icon
+          'icon-size': 1.5,
+        },
+      });
+
+      // Add the heart icon to the map
+      map.loadImage(
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Heart_corazón.svg/1024px-Heart_corazón.svg.png',
+        (error, image) => {
+          if (error) throw error;
+          if (!map.hasImage('heart-icon')) {
+            map.addImage('heart-icon', image);
+          }
+        }
+      );
     });
 
     return () => map.remove(); // Clean up on component unmount
