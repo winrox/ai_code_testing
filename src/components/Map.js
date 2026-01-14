d0e27508a8f4b6107af36be8ae07c47bdd5471df
Winnie Palangpour <winnie.mabee@gmail.com>
2026-01-14 09:24:30 -0600

    CoPilot 'Using assets/veloway.geojson as a source add a new layer to the Map component that shows up when at zoom level 11 or higher.'

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import veloway from '../assets/veloway.geojson';
 
 const MapComponent = () => {
   const mapContainerRef = useRef(null);
@@ -74,6 +75,27 @@ const MapComponent = () => {
         }
       });
 
+      // Add veloway geojson as a source and show as a line at zoom 11+
+      map.addSource('veloway', {
+        type: 'geojson',
+        data: veloway
+      });
+
+      map.addLayer({
+        id: 'veloway-layer',
+        type: 'line',
+        source: 'veloway',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#ff0077',
+          'line-width': 3,
+          'line-opacity': 0.9
+        },
+        minzoom: 11
+      });
     });
 
     return () => map.remove(); // Clean up on component unmount
